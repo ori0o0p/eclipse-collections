2394c2b71c5f8a6f5285ac97c85c5515bd9867eb
101 <tmddnjs001133@dsm.hs.kr>
2023-11-27 15:39:30 +0900

    setting

---
+++ src/main/java/com/example/eclipsecollection/EclipseCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.eclipsecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipseCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EclipseCollectionApplication.class, args);
+    }
+
+}
