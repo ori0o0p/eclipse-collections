023f6ed8e0641237d2d6730585d50de5bf59a0ae
101 <tmddnjs001133@dsm.hs.kr>
2023-11-27 21:48:57 +0900

    List 예제

---
+++ src/main/java/com/example/eclipsecollection/collection/List.java
@@ -0,0 +1,37 @@
+package com.example.eclipsecollection.collection;
+
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.api.list.MutableList;
+
+public class List {
+
+    public static void main(String[] args) {
+        MutableList<String> emptyList =
+                Lists.mutable.empty();
+
+        emptyList.each(System.out::print);
+
+        System.out.println();
+
+        MutableList<String> list =
+                Lists.mutable.with( "1" , "2" , "3" );
+
+        list.each(System.out::print);
+
+        System.out.println();
+
+        ImmutableList<String> emptyImmutableList =
+                Lists.immutable.empty();
+
+        emptyImmutableList.each(System.out::print);
+
+        System.out.println();
+
+        ImmutableList<String> immutableList =
+                Lists.immutable.with( "1" , "2" , "3" );
+
+        immutableList.each(System.out::print);
+    }
+
+}
